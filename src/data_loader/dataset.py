127182c06f7a5f09447a5833557a415d9bcd3cf4
Krzysztof <71060235+Krzysztof-Switek@users.noreply.github.com>
2025-06-14 12:44:52 +0200

    augment utils added - dataset split into two files

--- src/data_loader/dataset.py
+++ src/data_loader/dataset.py
@@ -7,59 +7,13 @@ from collections import Counter, defaultdict
 from omegaconf import DictConfig
 from pathlib import Path
 from utils.path_manager import PathManager
-
-
-class AugmentWrapper(torch.utils.data.Dataset):
-    def __init__(self, base_dataset, metadata, class_counts, max_count, transform_base, transform_strong, augment_applied):
-        self.base_dataset = base_dataset
-        self.metadata = metadata
-        self.class_counts = class_counts
-        self.max_count = max_count
-        self.transform_base = transform_base
-        self.transform_strong = transform_strong
-        self.augment_applied = augment_applied
-
-        self._printed_augmentation_notice = False
-
-    def __len__(self):
-        return len(self.base_dataset)
-
-    def __getitem__(self, index):
-        path, label = self.base_dataset.samples[index]
-        image = self.base_dataset.loader(path)
-
-        fname = os.path.basename(path).strip().replace(" ", "_").lower()
-
-        if fname not in self.metadata:
-            print(f"⚠️ Nie znaleziono metadanych dla pliku: {fname}")
-            transform = self.transform_base
-            return transform(image), label, {"populacja": torch.tensor(-1), "wiek": torch.tensor(-1)}
-
-        pop, wiek = self.metadata[fname]
-        count = self.class_counts.get((pop, wiek), 0)
-
-        desired_total = self.max_count
-        augment_needed = max(0, desired_total - count)
-        prob = min(1.0, augment_needed / desired_total)
-
-        if torch.rand(1).item() < prob:
-            self.augment_applied[(pop, wiek)] += 1
-            transform = self.transform_strong
-
-            if not self._printed_augmentation_notice:
-                print("✨ Augmentacja w toku dla klas o małej liczności...")
-                self._printed_augmentation_notice = True
-        else:
-            transform = self.transform_base
-
-        return transform(image), label, {"populacja": torch.tensor(pop), "wiek": torch.tensor(wiek)}
+from engine.augment_utils import AugmentWrapper
 
 
 class HerringDataset:
     def __init__(self, config: DictConfig):
         self.cfg = config
         self.path_manager = PathManager(Path(__file__).parent.parent.parent, config)
-
         self.train_transform_base = self._get_base_transforms()
         self.train_transform_strong = self._get_strong_transforms()
         self.val_transform = self._get_val_transforms()
@@ -70,6 +24,7 @@ class HerringDataset:
 
         print(f"\n📊 Największa liczność klas (populacja, wiek): {self.max_count}")
         self._validate_labels()
+
 
     def _load_metadata(self):
         excel_path = self.path_manager.metadata_file()
---
+++ src/engine/augment_utils.py
@@ -0,0 +1,48 @@
+import os
+import torch
+
+
+class AugmentWrapper(torch.utils.data.Dataset):
+    def __init__(self, base_dataset, metadata, class_counts, max_count, transform_base, transform_strong, augment_applied):
+        self.base_dataset = base_dataset
+        self.metadata = metadata
+        self.class_counts = class_counts
+        self.max_count = max_count
+        self.transform_base = transform_base
+        self.transform_strong = transform_strong
+        self.augment_applied = augment_applied
+
+        self._printed_augmentation_notice = False
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+    def __getitem__(self, index):
+        path, label = self.base_dataset.samples[index]
+        image = self.base_dataset.loader(path)
+
+        fname = os.path.basename(path).strip().replace(" ", "_").lower()
+
+        if fname not in self.metadata:
+            print(f"⚠️ Nie znaleziono metadanych dla pliku: {fname}")
+            transform = self.transform_base
+            return transform(image), label, {"populacja": torch.tensor(-1), "wiek": torch.tensor(-1)}
+
+        pop, wiek = self.metadata[fname]
+        count = self.class_counts.get((pop, wiek), 0)
+
+        desired_total = self.max_count
+        augment_needed = max(0, desired_total - count)
+        prob = min(1.0, augment_needed / desired_total)
+
+        if torch.rand(1).item() < prob:
+            self.augment_applied[(pop, wiek)] += 1
+            transform = self.transform_strong
+
+            if not self._printed_augmentation_notice:
+                print("✨ Augmentacja w toku dla klas o małej liczności...")
+                self._printed_augmentation_notice = True
+        else:
+            transform = self.transform_base
+
+        return transform(image), label, {"populacja": torch.tensor(pop), "wiek": torch.tensor(wiek)}
